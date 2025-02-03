61a3953ad90106d85bc269c924b4400a04cb7ce5
Maria Granli Nielsen <143001624+mariagn01@users.noreply.github.com>
2025-02-03 21:26:06 +0100

    farger

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,8 @@ export default {
           'jersey': ['"Jersey 10"'],
         },
         colors: {
-          'retro-beige': '#F5F5DC',
-          'retro-black': '#000000',
+          'retro-beige': '#eee4e1',
+          'black': '#000000',
         },
         animation: {
           'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
