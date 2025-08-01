7f4fad9bb36f8270a9f99e9ad6f424dff1dedd09
Maria Granli Nielsen <143001624+mariagn01@users.noreply.github.com>
2025-08-01 14:54:17 +0200

    Add font sources and update Hero section with personal introduction

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
 import Hero from './sections/hero'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme();
 
 function App() {
 
   return (
-    <Hero />
+    <ThemeProvider theme={theme}>
+      <Hero />
+    </ThemeProvider>
   );
 }
 
--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -1,9 +1,14 @@
+import { Container, Box } from '@mui/material';
+
 export default function Hero() {
   return (
-    <section className="hero">
-      <div className="hero-content">
-        <h1>Hei verden!</h1>
-      </div>
-    </section>
+      <Container maxWidth="lg" sx={{ display: 'flex', alignItems: 'flex-start', flexDirection: 'column', mt: 10 }}>
+          <h1>Maria Granli</h1>
+          <p>I’m a software engineering student from Oslo, currently in my
+              third year, working on developing my technical understanding and
+              skills. I also have experience in web development and graphic
+              design from my previous bachelor’s degree in media and
+              communication. </p>
+      </Container>
   );
 }
